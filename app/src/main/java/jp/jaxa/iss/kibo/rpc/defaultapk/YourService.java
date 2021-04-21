f10b4c3131067e078d930c36493f09bfe1faa956
NotThatUwU <72650902+NotThatUwU@users.noreply.github.com>
2021-04-21 13:44:50 +0700

    Added MoveTo Method and TakeBitmapNavCamPicture Method

--- app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/YourService.java
@@ -1,5 +1,9 @@
 package jp.jaxa.iss.kibo.rpc.defaultapk;
 
+import android.graphics.Bitmap;
+
+import gov.nasa.arc.astrobee.types.Point;
+import gov.nasa.arc.astrobee.types.Quaternion;
 import jp.jaxa.iss.kibo.rpc.api.KiboRpcService;
 
 /**
@@ -12,7 +16,11 @@ public class YourService extends KiboRpcService {
         //Start
         api.startMission();
 
-        //Write code here
+        //Move to first pos
+        moveTo(11.21, -9.8, 4.79, 0, 0, 0, 1, true);
+        //(read QR, move to Point-A’, read AR and aim the target)
+        takeBitmapNavCamPicture();
+        //Move to pos 2
 
         //Stop
         api.reportMissionCompletion();
@@ -26,6 +34,23 @@ public class YourService extends KiboRpcService {
     @Override
     protected void runPlan3(){
         // write here your plan 3
+    }
+
+    private void moveTo(double pointX, double pointY, double pointZ, float quaternionX, float quaternionY, float quaternionZ, float quaternionW, boolean printRobotPos){
+        Point point = new Point(pointX, pointY, pointZ);
+        Quaternion quaternion = new Quaternion(quaternionX, quaternionY, quaternionZ, quaternionW);
+        api.moveTo(point, quaternion, printRobotPos);
+    }
+
+    private void takeBitmapNavCamPicture(){
+        Bitmap image = api.getBitmapNavCam();
+        String content = readQR(image);
+        api.sendDiscoveredQR(content); // send the content of QR code for judge.
+    }
+
+    private String readQR(Bitmap image){
+        String code = null;//Noting yet for now
+        return code;
     }
 
 }
